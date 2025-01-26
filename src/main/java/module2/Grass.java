3bc6c940954d86814a428302d034a2bbf0b42872
AloneProgramist <momoroot33@gmail.con>
2025-01-26 19:18:07 +0200

    lol

--- src/main/java/module2/Grass.java
+++ src/main/java/module2/Grass.java
@@ -3,7 +3,7 @@ package module2;
 import java.util.Map;
 
 public class Grass extends Animal {
-    public Grass(AnimalsNames name, Map<AnimalsNames, Double> mapEat, int weight, int height) {
+    public Grass(AnimalsNames name,  int weight, int height) {
         Name  = name;
         eatMap =  mapEat;
         Height = height;
